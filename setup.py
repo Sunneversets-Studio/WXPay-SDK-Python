cca025d6d7a827960ca802d2ace6e4b82382d32c
letiantian <sunlt1699@gmail.com>
2016-11-15 11:25:46 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -21,12 +21,16 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Utilities',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
     install_requires=[
-        'lazyxml',
+        'xmltodict',
         'requests'
     ],
 
